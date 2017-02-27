60ad297781614b2abf4328549a2a07145e01f1f4
anthonny <anthonny.querouil@gmail.com>
2017-02-28 00:42:10 +0100

    feat(context): libs installed and data/images added

---
+++ src/data/places.data.js
@@ -0,0 +1,77 @@
+/*jshint unused: false */
+var njPlaces = [
+  {
+    id: 'dojo',
+    name: 'DOJO Nantes',
+    address: '26 Boulevard de Stalingrad',
+    city: '44000 Nantes',
+    maps: 'https://goo.gl/maps/YMT6a7v6gSo',
+    capacity: 30
+  },
+  {
+    id: 'epsi',
+    name: 'EPSI Nantes',
+    address: '2 Rue Fénelon',
+    city: '44000 Nantes',
+    maps: 'http://goo.gl/maps/EO03M',
+    capacity: 70
+  },
+  {
+    id: 'cantine',
+    name: 'Cantine numérique',
+    address: '11 Impasse Juton',
+    city: '44000 Nantes',
+    maps: 'http://goo.gl/maps/0Swz1',
+    capacity: 40
+  },
+  {
+    id: 'netapsys',
+    name: 'Netapsys',
+    address: '17 Rue Sanlecque',
+    city: '44000 Nantes',
+    capacity: 'http://goo.gl/maps/Ctujp',
+    capacity: 50
+  },
+  {
+    id: 'sup_info',
+    name: 'Sup Info',
+    address: '1 Rue Charron',
+    city: '44800 Saint-Herblain',
+    capacity: 'http://goo.gl/maps/YbjKy'
+  },
+  {
+    id: 'eartsup',
+    name: 'e-artsup / Epitech',
+    address: '16-18 Rue Flandres Dunkerque',
+    city: '44000 Nantes',
+    maps: 'https://goo.gl/maps/tjQxgmswrVD2',
+    twitter: 'EPITECHNantes',
+    capacity: 130
+  },
+  {
+    id: 'epitech',
+    name: 'Epitech',
+    address: '5 Rue d\'Alger',
+    city: '44000 Nantes',
+    maps: 'https://goo.gl/maps/yRLvMRkNuzj',
+    twitter: 'EPITECHNantes',
+    capacity: 80
+  },
+  {
+    id: 'hub_creatic',
+    name: 'Hub Creatic',
+    address: '6 Rue Rose Dieng-Kuntz',
+    city: '44300 Nantes',
+    maps: 'https://goo.gl/maps/ySfUp',
+    twitter: 'hubcreatic',
+    capacity: 80
+  },
+  {
+	    id: 'insula',
+	    name: 'Insula',
+	    address: '47 rue de la Tour d\'Auvergne',
+	    city: '44000 Nantes',
+	    maps: 'https://goo.gl/maps/kF6xeKsUBZU2',
+	    capacity: 100
+	  }
+];
